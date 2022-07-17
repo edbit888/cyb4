b756c91617dd5935f8e762a570db0064748045ac
edbit888 <108486875+edbit888@users.noreply.github.com>
2022-07-17 22:35:12 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -8,16 +8,20 @@
 				margin: 5px;
 				text-align: center;
 			}
-		</style>	
+		</style>
+	</head>
+	
 	</head>
 	
 	<body>
 		<a href="index1.html">Домашняя страница</a>
-		<h1> Введите свои имя и пароль</h1>
-		<form method="POST"  action="check_login.php">
-			<input type="text" name="txtUser"/> <br />
-			<input type="password" name="txtPwd" /> <br />
-			<button>Вперед</button> <br />
-		</form>
+			<h1>Регистрация пользователя</h1>
+			<form name="forma" action="connect.php" method="post">
+ 				Ваше имя: <input type="text" name="name"><br />
+ 				Пароль:   <input type="text" name="pwd"><br />
+ 				Ваш E-mail: <input type="text" name="email"><br />
+				<input name="submit" type="submit" value="Отправить">
+			<h1>Выполнил Ярушин Эдуард</h1>
+			</form>
 	</body>
 </html>
